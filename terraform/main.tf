6a3c36030fcbb1b0b33715880f6f89aac5d0676e
Sacha Wharton <extern_wharton_sacha@allianz.de>
2022-11-06 12:09:08 +0200

    🐛 FIX:

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,30 +101,7 @@ resource "helm_release" "argocd" {
   create_namespace = true
   depends_on = [kind_cluster.default]
 }
-
-#resource "null_resource" "helm_install" {
-#  triggers = {
-#    key = uuid()
-#  }
-#
-#    provisioner "local-exec" {
-#      command = <<EOF
-#      #!/bin/bash
-#      set -euo pipefail
-#      echo "Adding & installing Keptn Ortelius Service & Ortelius"
-#      helm repo add keptn-ortelius-service https://ortelius.github.io/keptn-ortelius-service
-#      helm repo add ortelius https://ortelius.github.io/ortelius-charts/
-#      helm install my-ms-dep-pkg-cud ortelius/ms-dep-pkg-cud --version 10.0.0-build.66
-#      helm install my-ms-compitem-crud ortelius/ms-compitem-crud --version 10.0.0-build.78
-#      helm install my-ms-dep-pkg-r ortelius/ms-dep-pkg-r --version 10.0.0-build.53
-#      helm install my-ms-validate-user ortelius/ms-validate-user --version 10.0.0-build.48
-#      helm install my-keptn-ortelius-service keptn-ortelius-service/keptn-ortelius-service --version 0.0.1
-#      echo "Done"
-#      EOF
-#    }
-#    depends_on = [kind_cluster.default]
-#}
-
+ß
 resource "helm_release" "keptn" {
   name = "keptn"
 
