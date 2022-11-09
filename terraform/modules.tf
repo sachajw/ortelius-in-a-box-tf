0e26d654d9e03973256aec4f23f09b1b2d5099cf
Sacha Wharton <extern_wharton_sacha@allianz.de>
2022-11-09 21:54:26 +0200

    🐛 FIX:

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -2,8 +2,7 @@ module "istio" {
   source                          = "sepulworld/istio/helm"
   version                         = "0.0.3"
   cluster_name                    = "ortelius-in-a-box"
-  create_namespace                = true
-  cleanup_on_fail                 = true
+  create_namespace                = false
   force_update                    = true
   helm_repo_url                   = "https://istio-release.storage.googleapis.com/charts"
   istiod_meshConfig_accessLogFile = "/dev/stdout"
@@ -13,11 +12,11 @@ module "istio" {
 resource "helm_release" "istio_base" {
   name  = "istio"
   chart = "base"
-  #  cleanup_on_fail = true
+  #cleanup_on_fail = true
 }
 
 resource "helm_release" "istio_istiod" {
   name  = "istio"
   chart = "istiod"
-  #  cleanup_on_fail = true
+  #cleanup_on_fail = true
 }
