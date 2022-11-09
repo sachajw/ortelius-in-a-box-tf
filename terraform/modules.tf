628d7e9a087738e369f7bb53896b9dcb5612719a
Sacha Wharton <extern_wharton_sacha@allianz.de>
2022-11-09 21:45:49 +0200

    🐛 FIX:

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,30 +1,23 @@
 module "istio" {
-  source       = "sepulworld/istio/helm"
-  version      = "0.0.3"
-  cluster_name = "ortelius-in-a-box"
+  source                          = "sepulworld/istio/helm"
+  version                         = "0.0.3"
+  cluster_name                    = "ortelius-in-a-box"
+  create_namespace                = true
+  cleanup_on_fail                 = true
+  force_update                    = true
+  helm_repo_url                   = "https://istio-release.storage.googleapis.com/charts"
+  istiod_meshConfig_accessLogFile = "/dev/stdout"
+  recreate_pods                   = false
 }
 
 resource "helm_release" "istio_base" {
-  name       = "istio"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "base"
-  #timeout         = 120
-  cleanup_on_fail = true
-  force_update    = false
-  namespace       = "istio-system"
+  name  = "istio"
+  chart = "base"
+  #  cleanup_on_fail = true
 }
 
 resource "helm_release" "istio_istiod" {
-  name       = "istio"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "istiod"
-  #timeout         = 120
-  cleanup_on_fail = true
-  force_update    = false
-  namespace       = "istio-system"
-
-  set {
-    name  = "meshConfig.accessLogFile"
-    value = "/dev/stdout"
-  }
+  name  = "istio"
+  chart = "istiod"
+  #  cleanup_on_fail = true
 }
