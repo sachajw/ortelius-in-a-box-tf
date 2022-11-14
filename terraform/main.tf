2bfbf92c818b171ced5f4fdf436742dbfed864d0
Sacha Wharton <extern_wharton_sacha@allianz.de>
2022-11-14 17:54:25 +0200

    🐛 FIX:

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,9 +55,9 @@ resource "null_resource" "kind_copy_container_images" {
 
   provisioner "local-exec" {
     command = <<EOF
-      kind load docker-image --name ortelius-in-a-box --nodes ortelius-in-a-box-control-plane,ortelius-in-a-box-worker docker.io/arangodb/arangodb:latest
       kind load docker-image --name ortelius-in-a-box --nodes ortelius-in-a-box-control-plane,ortelius-in-a-box-worker quay.io/ortelius/ortelius
       kind load docker-image --name ortelius-in-a-box --nodes ortelius-in-a-box-control-plane,ortelius-in-a-box-worker ghcr.io/ortelius/keptn-ortelius-service:0.0.2-dev
+      kind load docker-image --name ortelius-in-a-box --nodes ortelius-in-a-box-control-plane,ortelius-in-a-box-worker docker.io/istio/base:1.16-2022-11-02T13-31-52
     EOF
   }
 }
