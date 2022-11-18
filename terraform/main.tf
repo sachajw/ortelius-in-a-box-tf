46294ea1b0f6a3f9b7f0a3464b9c298c4bd9a84e
Sacha Wharton <extern_wharton_sacha@allianz.de>
2022-11-18 21:45:00 +0200

    🐛 FIX:

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,8 +108,8 @@ resource "helm_release" "kube_arangodb" {
 # nginx ingress
 resource "helm_release" "nginx_ingress" {
   name             = "nginx-ingress"
-  repository       = "https://helm.nginx.com/stable"
-  chart            = "nginx-ingress"
+  repository       = "https://github.com/kubernetes/ingress-nginx"
+  chart            = "ingress-nginx"
   namespace        = var.nginx_ingress_namespace
   create_namespace = true
 
