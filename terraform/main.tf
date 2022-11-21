d336a99a3a5957a777929ba4b08f193c75b926b5
Sacha Wharton <extern_wharton_sacha@allianz.de>
2022-11-21 23:07:12 +0200

    🐛 FIX:

--- terraform/main.tf
+++ terraform/main.tf
@@ -169,6 +169,7 @@ resource "helm_release" "ortelius" {
   namespace        = "ortelius"
   create_namespace = true
   depends_on       = [kind_cluster.ortelius]
+  timeout          = 600
 
   set {
     name  = "ms-general.dbpass"
